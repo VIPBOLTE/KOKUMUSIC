f742b95ffd3abec34ea012047fc676d4dd8d8d65
MR GOKU <154590485+VIPBOLTE@users.noreply.github.com>
2025-02-11 02:52:49 +0530

    Update sangmata.py

--- KOKUMUSIC/plugins/user/sangmata.py
+++ KOKUMUSIC/plugins/user/sangmata.py
@@ -30,8 +30,8 @@ async def sg(client: Client, message: Message):
             return await lol.edit("<code>Please specify a valid user!</code>")
     sgbot = ["sangmata_bot", "sangmata_beta_bot"]
     sg = random.choice(sgbot)
-    BAD = random.choice(assistants)
-    ubot = await get_client(BAD)
+    KOKU = random.choice(assistants)
+    ubot = await get_client(KOKU)
 
     try:
         a = await ubot.send_message(sg, f"{user.id}")
