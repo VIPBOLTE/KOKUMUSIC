b5ce28bab279cf494e0ea2a2bc86eac58850ee1b
MR GOKU <154590485+VIPBOLTE@users.noreply.github.com>
2025-02-11 02:03:04 +0530

    Update call.py

--- KOKUMUSIC/core/call.py
+++ KOKUMUSIC/core/call.py
@@ -577,4 +577,41 @@ class Call(PyTgCalls):
         if config.STRING4:
             pings.append(self.four.ping)
         if config.STRING5:
- 
+             await self.five.start()
+
+    
+    async def decorators(self):
+        @self.one.on_update(filters.chat_update(ChatUpdate.Status.KICKED))
+        @self.two.on_update(filters.chat_update(ChatUpdate.Status.KICKED))
+        @self.three.on_update(filters.chat_update(ChatUpdate.Status.KICKED))
+        @self.four.on_update(filters.chat_update(ChatUpdate.Status.KICKED))
+        @self.five.on_update(filters.chat_update(ChatUpdate.Status.KICKED))
+        
+
+        @self.one.on_update(filters.chat_update(ChatUpdate.Status.CLOSED_VOICE_CHAT))
+        @self.two.on_update(filters.chat_update(ChatUpdate.Status.CLOSED_VOICE_CHAT))
+        @self.three.on_update(filters.chat_update(ChatUpdate.Status.CLOSED_VOICE_CHAT))
+        @self.four.on_update(filters.chat_update(ChatUpdate.Status.CLOSED_VOICE_CHAT))
+        @self.five.on_update(filters.chat_update(ChatUpdate.Status.CLOSED_VOICE_CHAT))
+        
+            
+        @self.one.on_update(filters.chat_update(ChatUpdate.Status.LEFT_GROUP))
+        @self.two.on_update(filters.chat_update(ChatUpdate.Status.LEFT_GROUP))
+        @self.three.on_update(filters.chat_update(ChatUpdate.Status.LEFT_GROUP))
+        @self.four.on_update(filters.chat_update(ChatUpdate.Status.LEFT_GROUP))
+        @self.five.on_update(filters.chat_update(ChatUpdate.Status.LEFT_GROUP))
+        async def stream_services_handler(_, chat_id: int):
+            await self.stop_stream(chat_id)
+
+        
+        @self.one.on_update(filters.stream_end)
+        @self.two.on_update(filters.stream_end)
+        @self.three.on_update(filters.stream_end)
+        @self.four.on_update(filters.stream_end)
+        @self.five.on_update(filters.stream_end)
+        async def stream_end_handler1(client, update: Update):
+            if not isinstance(update, StreamAudioEnded):
+                return
+            await self.change_stream(client, update.chat_id)
+
+KOKU = Call()
