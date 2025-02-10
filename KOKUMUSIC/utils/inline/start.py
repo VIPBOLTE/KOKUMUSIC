b2d323f6d019cd1c6e63326108a579140e5d669b
MR GOKU <154590485+VIPBOLTE@users.noreply.github.com>
2025-02-11 00:34:02 +0530

    Create start.py

---
+++ KOKUMUSIC/utils/inline/start.py
@@ -0,0 +1,87 @@
+from pyrogram.types import InlineKeyboardButton, WebAppInfo
+from config import SUPPORT_CHAT, SUPPORT_CHANNEL
+from KOKUMUSIC import app
+
+def start_pannel(_):
+    buttons = [
+        [
+            InlineKeyboardButton(
+                text="💠 𝐀𝙳𝙳 𝙼𝙴 𝙸𝙽 𝙽𝙴𝚆 𝙶𝚁𝙾𝚄𝙿𝚂 💠",
+                url=f"https://t.me/{app.username}?startgroup=true",
+            ),
+        ],
+        [
+            InlineKeyboardButton(text="۞ 𝐇𝙴𝙻𝙿 ۞", callback_data="settings_back_helper"),
+            InlineKeyboardButton(text="☢ 𝐒𝙴𝚃 ☢", callback_data="settings_helper"),
+        ],
+        [
+            InlineKeyboardButton(text="✡ 𝐆𝚁𝙾𝚄𝙿 ✡", url=f"https://t.me/{SUPPORT_CHAT}"),
+        ],
+    ]
+    return buttons
+
+
+def private_panel(_):
+    buttons = [
+        [
+            InlineKeyboardButton(
+                text="💠 𝐀𝙳𝙳 𝙼𝙴 𝙸𝙽 𝙽𝙴𝚆 𝙶𝚁𝙾𝚄𝙿𝚂 💠",
+                url=f"https://t.me/{app.username}?startgroup=true",
+            )
+        ],
+        [
+            InlineKeyboardButton(text="𝐆𝚁𝙾𝚄𝙿✨", url=f"https://t.me/{SUPPORT_CHAT}"),
+            InlineKeyboardButton(text="𝐌ᴏʀᴇ🥀", url=f"https://t.me/{SUPPORT_CHANNEL}"),
+        ],
+        [
+            InlineKeyboardButton(
+                text="۞ 𝐅𝙴𝙰𝚃𝚄𝚁𝙴𝚂 ۞", callback_data="settings_back_helper"
+            )
+        ],
+    ]
+    return buttons
+
+
+def alive_panel(_):
+    buttons = [
+            [
+            InlineKeyboardButton(
+                text="📩 ꜱᴛᴀʀᴛ ᴘᴍ",
+                url=f"https://t.me/{app.username}",
+            ),
+        ],
+        [
+            InlineKeyboardButton(
+                text="✿︎ ᴀᴅᴅ ᴍᴇ ✿︎", url=f"https://t.me/{app.username}?startgroup=true"
+            ),
+            InlineKeyboardButton(text=_["S_B_3"], url=f"https://t.me/{SUPPORT_CHAT}"),
+        ],
+                [
+            InlineKeyboardButton(text="🎧 ꜱᴘᴏᴛɪꜰʏ", web_app=WebAppInfo(url="https://open.spotify.com/"),)
+            
+        ],
+    ]
+    return buttons
+    
+
+
+def music_start_panel(_):
+    buttons = [
+        [
+            InlineKeyboardButton(
+                text="▪️ᴀᴅᴅ ᴍᴇ ɪɴ ɴᴇᴡ ɢʀᴏᴜᴘ▪️",
+                url=f"https://t.me/{app.username}?startgroup=true",
+            )
+        ],
+        [
+            InlineKeyboardButton(text="▪️ᴀʙᴏᴜᴛ▪️", callback_data="about"),
+            InlineKeyboardButton(text="▪️sᴜᴘᴘᴏʀᴛ▪️", callback_data="support"),
+        ],
+        [
+            InlineKeyboardButton(text="▪️ꜱᴘᴏᴛɪꜰʏ▪️", web_app=WebAppInfo(url="https://open.spotify.com/"),)
+            
+        ],
+        [InlineKeyboardButton(text="▪️ ғᴇᴀᴛᴜʀᴇ ▪️", callback_data="feature")],
+        
+    ]
+    return buttons
