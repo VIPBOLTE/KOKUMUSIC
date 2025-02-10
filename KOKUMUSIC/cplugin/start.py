e212af6ab24204828ea448617d9f3755ff07b40c
MR GOKU <154590485+VIPBOLTE@users.noreply.github.com>
2025-02-11 01:29:54 +0530

    Create start.py

---
+++ KOKUMUSIC/cplugin/start.py
@@ -0,0 +1,12 @@
+from pyrogram import Client, filters
+from pyrogram.types import Message
+from KOKUMUSIC import app
+
+@Client.on_message(filters.command("start"))
+async def start_message(client: Client, message: Message):
+    await message.reply_text(
+        f"ʜᴇʟʟᴏ {message.from_user.first_name}! 👋\n\n"
+        "ɪ'ᴍ ʏᴏᴜʀ ʀᴇᴀᴄᴛɪᴏɴ ʙᴏᴛ! ɪ'ʟʟ ʀᴇᴀᴄᴛ ᴛᴏ ᴇᴠᴇʀʏ ᴍᴇꜱꜱᴀɢᴇ ɪɴ ɢʀᴏᴜᴘꜱ, ᴄʜᴀɴɴᴇʟꜱ, ᴀɴᴅ ᴘʀɪᴠᴀᴛᴇ ᴄʜᴀᴛꜱ ᴡɪᴛʜ ᴀ 👍 ᴇᴍᴏᴊɪ.\n\n"
+        "ᴀᴅᴅ ᴍᴇ ᴛᴏ ʏᴏᴜʀ ɢʀᴏᴜᴘ ᴏʀ ᴄʜᴀɴɴᴇʟ ᴀɴᴅ ᴡᴀᴛᴄʜ ᴍᴇ ɪɴ ᴀᴄᴛɪᴏɴ! 🚀\n\n"
+        "ʏᴏᴜ ᴄᴀɴ ᴍᴀᴋᴇ ʏᴏᴜʀ ʙᴏᴛ ʙʏ /clone😁"
+    )
