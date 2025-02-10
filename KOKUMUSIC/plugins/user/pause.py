ad2a1f44e990624d999e9773093ee1500d0704f3
MR GOKU <154590485+VIPBOLTE@users.noreply.github.com>
2025-02-11 03:14:39 +0530

    Update pause.py

--- KOKUMUSIC/plugins/user/pause.py
+++ KOKUMUSIC/plugins/user/pause.py
@@ -1,13 +1,11 @@
 from pyrogram import filters
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
-
 from pyrogram import filters, Client
 from config import BANNED_USERS
-from BADMUSIC import app
-from BADMUSIC.core.call import BAD as BAD
-from BADMUSIC.utils.database import is_music_playing, music_off
-from BADMUSIC.utils.decorators import AdminRightsCheck
-
+from KOKUMUSIC import app
+from KOKUMUSIC.core.call import KOKU
+from KOKUMUSIC.utils.database import is_music_playing, music_off
+from KOKUMUSIC.utils.decorators import AdminRightsCheck
 
 @Client.on_message(filters.command(["pause", "cpause"], prefixes=["."]) & filters.group & ~BANNED_USERS)
 @AdminRightsCheck
@@ -15,7 +13,7 @@ async def pause_admin(cli, message: Message, _, chat_id):
     if not await is_music_playing(chat_id):
         return await message.reply_text(_["admin_1"])
     await music_off(chat_id)
-    await BAD.pause_stream(chat_id)
+    await KOKU.pause_stream(chat_id)
 
     buttons = [
         [
