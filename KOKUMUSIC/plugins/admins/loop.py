cc59a94b51cab37d0a423e4b0d52fa147abdc8cf
MR GOKU <154590485+VIPBOLTE@users.noreply.github.com>
2025-02-11 03:57:27 +0530

    Update loop.py

--- KOKUMUSIC/plugins/admins/loop.py
+++ KOKUMUSIC/plugins/admins/loop.py
@@ -1,14 +1,11 @@
-# Copyright (C) 2024 by Badhacker98@Github, < https://github.com/Badhacker98 >.
-# Owner https://t.me/ll_BAD_MUNDA_ll
-
 from pyrogram import filters
 from pyrogram.types import Message
 
 from config import BANNED_USERS
 from strings import get_command
-from BADMUSIC import app
-from BADMUSIC.utils.database.memorydatabase import get_loop, set_loop
-from BADMUSIC.utils.decorators import AdminRightsCheck
+from KOKUMUSIC import app
+from KOKUMUSIC.utils.database.memorydatabase import get_loop, set_loop
+from KOKUMUSIC.utils.decorators import AdminRightsCheck
 
 # Commands
 LOOP_COMMAND = get_command("LOOP_COMMAND")
