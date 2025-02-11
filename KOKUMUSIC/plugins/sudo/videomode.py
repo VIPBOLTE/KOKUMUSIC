192c1b6beeccc18e55830ced299a4ea1ff5a6c7f
MR GOKU <154590485+VIPBOLTE@users.noreply.github.com>
2025-02-11 19:17:58 +0530

    Update videomode.py

--- KOKUMUSIC/plugins/sudo/videomode.py
+++ KOKUMUSIC/plugins/sudo/videomode.py
@@ -3,10 +3,10 @@ from pyrogram.types import Message
 
 import config
 from strings import get_command
-from BADMUSIC import app
-from BADMUSIC.misc import SUDOERS
-from BADMUSIC.utils.database import add_off, add_on
-from BADMUSIC.utils.decorators.language import language
+from KOKUMUSIC import app
+from KOKUMUSIC.misc import SUDOERS
+from KOKUMUSIC.utils.database import add_off, add_on
+from KOKUMUSIC.utils.decorators.language import language
 
 # Commands
 VIDEOMODE_COMMAND = get_command("VIDEOMODE_COMMAND")
