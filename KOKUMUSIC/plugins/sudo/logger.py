4e98ee2802033e32f990a003af35d9390c14c236
MR GOKU <154590485+VIPBOLTE@users.noreply.github.com>
2025-02-11 19:28:05 +0530

    Update logger.py

--- KOKUMUSIC/plugins/sudo/logger.py
+++ KOKUMUSIC/plugins/sudo/logger.py
@@ -2,10 +2,10 @@ from pyrogram import filters
 
 import config
 from strings import get_command
-from BADMUSIC import app
-from BADMUSIC.misc import SUDOERS
-from BADMUSIC.utils.database import add_off, add_on
-from BADMUSIC.utils.decorators.language import language
+from KOKUMUSIC import app
+from KOKUMUSIC.misc import SUDOERS
+from KOKUMUSIC.utils.database import add_off, add_on
+from KOKUMUSIC.utils.decorators.language import language
 
 # Commands
 LOGGER_COMMAND = get_command("LOGGER_COMMAND")
