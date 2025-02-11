a27269665d0299e5a90dac851c927b552027a2bd
MR GOKU <154590485+VIPBOLTE@users.noreply.github.com>
2025-02-12 00:26:35 +0530

    Update globalban.py

--- KOKUMUSIC/plugins/sudo/globalban.py
+++ KOKUMUSIC/plugins/sudo/globalban.py
@@ -144,9 +144,9 @@ from pyrogram.enums import MessageEntityType
 from pyrogram.types import Message, User
 
 from config import BANNED_USERS
-from BADMUSIC import app
-from BADMUSIC.misc import SUDOERS
-from BADMUSIC.utils.database import add_banned_user, is_banned_user, remove_banned_user
+from KOKUMUSIC import app
+from KOKUMUSIC.misc import SUDOERS
+from KOKUMUSIC.utils.database import add_banned_user, is_banned_user, remove_banned_user
 
 
 async def extract_user(m: Message) -> User:
