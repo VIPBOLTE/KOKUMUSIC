86369c7e2d120a39f164d30612f6cfae55b6ee89
MR GOKU <154590485+VIPBOLTE@users.noreply.github.com>
2025-02-11 19:35:17 +0530

    Update block.py

--- KOKUMUSIC/plugins/sudo/block.py
+++ KOKUMUSIC/plugins/sudo/block.py
@@ -3,10 +3,10 @@ from pyrogram.types import Message
 
 from config import BANNED_USERS
 from strings import get_command
-from BADMUSIC import app
-from BADMUSIC.misc import SUDOERS
-from BADMUSIC.utils.database import add_gban_user, remove_gban_user
-from BADMUSIC.utils.decorators.language import language
+from KOKUMUSIC import app
+from KOKUMUSIC.misc import SUDOERS
+from KOKUMUSIC.utils.database import add_gban_user, remove_gban_user
+from KOKUMUSIC.utils.decorators.language import language
 
 # Command
 BLOCK_COMMAND = get_command("BLOCK_COMMAND")
