aac63f4a9f1fc4a84c080eb64aa54bdb8e18e11a
MR GOKU <154590485+VIPBOLTE@users.noreply.github.com>
2025-02-12 04:20:58 +0530

    Update __main__.py

--- KOKUMUSIC/__main__.py
+++ KOKUMUSIC/__main__.py
@@ -1,12 +1,15 @@
 import asyncio
 import importlib
+
 from pyrogram import idle
+
 import config
 from config import BANNED_USERS
 from KOKUMUSIC import HELPABLE, LOGGER, app, userbot
 from KOKUMUSIC.core.call import KOKU
 from KOKUMUSIC.plugins import ALL_MODULES
 from KOKUMUSIC.utils.database import get_banned_users, get_gbanned
+
 
 async def init():
     if (
@@ -24,6 +27,7 @@ async def init():
         LOGGER("KOKUMUSIC").warning(
             "No Spotify Vars defined. Your bot won't be able to play spotify queries."
         )
+
     try:
         users = await get_gbanned()
         for user_id in users:
@@ -36,7 +40,6 @@ async def init():
     await app.start()
     for all_module in ALL_MODULES:
         imported_module = importlib.import_module(all_module)
-
         if hasattr(imported_module, "__MODULE__") and imported_module.__MODULE__:
             if hasattr(imported_module, "__HELP__") and imported_module.__HELP__:
                 HELPABLE[imported_module.__MODULE__.lower()] = imported_module
@@ -46,6 +49,8 @@ async def init():
     await KOKU.decorators()
     LOGGER("KOKUMUSIC").info("KOKUMUSIC STARTED SUCCESSFULLY 🕊️")
     await idle()
+
+
 if __name__ == "__main__":
     asyncio.get_event_loop_policy().get_event_loop().run_until_complete(init())
     LOGGER("KOKUMUSIC").info("Stopping KOKUMUSIC! GoodBye")
