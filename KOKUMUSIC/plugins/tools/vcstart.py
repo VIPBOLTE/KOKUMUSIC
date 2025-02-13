bdb3f00876b544b93e49a61c2fd5ed1dfbc78f78
MR GOKU <154590485+VIPBOLTE@users.noreply.github.com>
2025-02-14 00:31:18 +0530

    Update vcstart.py

--- KOKUMUSIC/plugins/tools/vcstart.py
+++ KOKUMUSIC/plugins/tools/vcstart.py
@@ -8,10 +8,10 @@ from pyrogram.raw.functions.phone import CreateGroupCall, DiscardGroupCall
 from pyrogram.raw.types import InputGroupCall, InputPeerChannel, InputPeerChat
 from pyrogram.types import ChatPrivileges, Message
 
-from ChampuMusic import app
-from ChampuMusic.core.call import Champu
-from ChampuMusic.utils.database import *
-from ChampuMusic.utils.database import set_loop
+from KOKUMUSIC import app
+from KOKUMUSIC.core.call import Champu
+from KOKUMUSIC.utils.database import *
+from KOKUMUSIC.utils.database import set_loop
 
 other_filters = filters.group & ~filters.via_bot & ~filters.forwarded
 other_filters2 = filters.private & ~filters.via_bot & ~filters.forwarded
