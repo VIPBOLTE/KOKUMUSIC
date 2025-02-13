bc44ad489dd31342e895585635c84fa530c0f87e
MR GOKU <154590485+VIPBOLTE@users.noreply.github.com>
2025-02-13 13:42:26 +0530

    Update logging.py

--- KOKUMUSIC/logging.py
+++ KOKUMUSIC/logging.py
@@ -5,6 +5,8 @@ from motor.motor_asyncio import AsyncIOMotorClient as MongoCli
 from logging.handlers import RotatingFileHandler
 from config import LOG_FILE_NAME
 import config 
+
+# Basic logging configuration
 logging.basicConfig(
     level=logging.INFO,
     format="[%(asctime)s - %(levelname)s] - %(name)s - %(message)s",
@@ -14,18 +16,28 @@ logging.basicConfig(
         logging.StreamHandler(),
     ],
 )
+
+# Set the logging level for specific libraries to ERROR
 logging.getLogger("pyrogram").setLevel(logging.ERROR)
-LOGGERR = logging.getLogger(__name__)
-boot = time.time()
-mongodb = MongoCli(config.MONGO_DB_URI)
-db = mongodb.Anonymous
-mongo = MongoClient(config.MONGO_DB_URI)
-OWNER = config.OWNER_ID
 logging.getLogger("apscheduler").setLevel(logging.ERROR)
 logging.getLogger("pytgcalls").setLevel(logging.ERROR)
 logging.getLogger("pymongo").setLevel(logging.ERROR)
 logging.getLogger("ntgcalls").setLevel(logging.ERROR)
 logging.getLogger("httpx").setLevel(logging.ERROR)
-logging.basicConfig(level=logging.DEBUG)
-def LOGGER(name: str) -> logging.Logger:
+
+# Main logger for the application
+LOGGERR = logging.getLogger(__name__)
+
+# MongoDB setup
+boot = time.time()
+mongodb = MongoCli(config.MONGO_DB_URI)
+db = mongodb.Anonymous
+mongo = MongoClient(config.MONGO_DB_URI)
+
+# App configuration
+OWNER = config.OWNER_ID
+
+# Function to get additional loggers
+def get_logger(name: str) -> logging.Logger:
     return logging.getLogger(name)
+
