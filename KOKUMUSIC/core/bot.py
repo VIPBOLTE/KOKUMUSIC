8eec85375d10607edf14a8aef27e2580d039a3c0
MR GOKU <154590485+VIPBOLTE@users.noreply.github.com>
2025-02-13 22:02:31 +0530

    Update bot.py

--- KOKUMUSIC/core/bot.py
+++ KOKUMUSIC/core/bot.py
@@ -19,11 +19,11 @@ import config
 from ..logging import LOGGER
 
 
-class ChampuBot(Client):
+class KOKUBOT(Client):
     def __init__(self):
         LOGGER(__name__).info(f"sᴛᴀʀᴛɪɴɢ ʙᴏᴛ...")
         super().__init__(
-            "ChampuMusic",
+            "KOKUMUSIC",
             api_id=config.API_ID,
             api_hash=config.API_HASH,
             bot_token=config.BOT_TOKEN,
