b68c9bfef1e1d4aab489492d857981cd20e221e5
MR GOKU <154590485+VIPBOLTE@users.noreply.github.com>
2025-02-13 19:07:41 +0530

    Update start.py

--- KOKUMUSIC/plugins/bot/start.py
+++ KOKUMUSIC/plugins/bot/start.py
@@ -36,7 +36,7 @@ from KOKUMUSIC.utils.inline import alive_panel, music_start_panel, start_pannel
 
 from .help import paginate_modules
 
-loop = asyncio.get_running_loop()
+loop = asyncio.get_event_loop()
 
 STICKER = [
     "CAACAgUAAx0CepnpNQABATUjZypavrymDoERINkF-M3u9JDQ6K8AAhoDAAIOnnlVpyrYiDnVgWYeBA",
