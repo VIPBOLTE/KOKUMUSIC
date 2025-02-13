aa7e7faa1ca761f28346e9c9e1606751fd439d29
MR GOKU <154590485+VIPBOLTE@users.noreply.github.com>
2025-02-14 04:35:03 +0530

    Update __init__.py

--- strings/__init__.py
+++ strings/__init__.py
@@ -1,20 +1,31 @@
 import os
 import sys
 from typing import List
+
 import yaml
+
 languages = {}
 commands = {}
+
 languages_present = {}
+
+
 def get_command(value: str) -> List:
     return commands["command"][value]
+
+
 def get_string(lang: str):
     return languages[lang]
+
+
 for filename in os.listdir(r"./strings"):
     if filename.endswith(".yml"):
         language_name = filename[:-4]
         commands[language_name] = yaml.safe_load(
             open(r"./strings/" + filename, encoding="utf8")
         )
+
+
 for filename in os.listdir(r"./strings/langs/"):
     if "en" not in languages:
         languages["en"] = yaml.safe_load(
@@ -33,8 +44,8 @@ for filename in os.listdir(r"./strings/langs/"):
                 languages[language_name][item] = languages["en"][item]
     try:
         languages_present[language_name] = languages[language_name]["name"]
-    except Exception as e:
-                import traceback 
-                print(f"Error loading {filename}: {e}")
-                traceback.print_exc()
-        
+    except:
+        print(
+            "There is some issue with the language file inside bot. Please report it to the TheChampu at @TheChampuClub on Telegram"
+        )
+        sys.exit()
