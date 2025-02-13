182e5112ec26eaec0678a6e26290503492b1a552
MR GOKU <154590485+VIPBOLTE@users.noreply.github.com>
2025-02-13 23:23:11 +0530

    Update queue.py

--- KOKUMUSIC/utils/stream/queue.py
+++ KOKUMUSIC/utils/stream/queue.py
@@ -2,7 +2,7 @@ from typing import Union
 
 from config import autoclean, chatstats, userstats
 from config.config import time_to_seconds
-from ChampuMusic.misc import db
+from KOKUMUSIC.misc import db
 
 
 async def put_queue(
