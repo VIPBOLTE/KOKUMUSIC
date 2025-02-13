f882b0ea106bddbcdce0c548821550061eb62068
MR GOKU <154590485+VIPBOLTE@users.noreply.github.com>
2025-02-14 00:15:23 +0530

    Update admins.py

--- KOKUMUSIC/utils/decorators/admins.py
+++ KOKUMUSIC/utils/decorators/admins.py
@@ -3,9 +3,9 @@ from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
 from config import adminlist
 from strings import get_string
-from ChampuMusic import app
-from ChampuMusic.misc import SUDOERS
-from ChampuMusic.utils.database import (
+from KOKUMUSIC import app
+from KOKUMUSIC.misc import SUDOERS
+from KOKUMUSIC.utils.database import (
     get_authuser_names,
     get_cmode,
     get_lang,
