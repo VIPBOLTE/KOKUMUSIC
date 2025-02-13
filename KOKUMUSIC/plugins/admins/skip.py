2492e6d29eb57aaa03b1647d549ec0215134192b
MR GOKU <154590485+VIPBOLTE@users.noreply.github.com>
2025-02-14 01:58:12 +0530

    Update skip.py

--- KOKUMUSIC/plugins/admins/skip.py
+++ KOKUMUSIC/plugins/admins/skip.py
@@ -49,7 +49,7 @@ async def skip(cli, message: Message, _, chat_id):
                                         ),
                                         disable_web_page_preview=True,
                                     )
-                                    await Champu.stop_stream(chat_id)
+                                    await KOKU.stop_stream(chat_id)
                                 except:
                                     return
                                 break
