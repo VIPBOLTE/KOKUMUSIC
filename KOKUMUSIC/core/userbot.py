f7fbcd702cbc30391db9f6cd68d23bd83c9c56aa
MR GOKU <154590485+VIPBOLTE@users.noreply.github.com>
2025-02-13 21:54:50 +0530

    Update userbot.py

--- KOKUMUSIC/core/userbot.py
+++ KOKUMUSIC/core/userbot.py
@@ -22,35 +22,35 @@ assistantids = []
 class Userbot(Client):
     def __init__(self):
         self.one = Client(
-            name="ChampuAss1",
+            name="KOKUAss1",
             api_id=config.API_ID,
             api_hash=config.API_HASH,
             session_string=str(config.STRING1),
             no_updates=False,
         )
         self.two = Client(
-            name="ChampuAss2",
+            name="KOKUAss2",
             api_id=config.API_ID,
             api_hash=config.API_HASH,
             session_string=str(config.STRING2),
             no_updates=True,
         )
         self.three = Client(
-            name="ChampuAss3",
+            name="KOKUAss3",
             api_id=config.API_ID,
             api_hash=config.API_HASH,
             session_string=str(config.STRING3),
             no_updates=True,
         )
         self.four = Client(
-            name="ChampuAss4",
+            name="KOKUAss4",
             api_id=config.API_ID,
             api_hash=config.API_HASH,
             session_string=str(config.STRING4),
             no_updates=True,
         )
         self.five = Client(
-            name="ChampuAss5",
+            name="KOKUAss5",
             api_id=config.API_ID,
             api_hash=config.API_HASH,
             session_string=str(config.STRING5),
@@ -63,10 +63,10 @@ class Userbot(Client):
         if config.STRING1:
             await self.one.start()
             try:
-                await self.one.join_chat("TheShivanshu")
-                await self.one.join_chat("TheChampuClub")
-                await self.one.join_chat("akaChampu")
-                await self.one.join_chat("TheChampu")
+                await self.four.join_chat("LOVING_SOCIETY")
+                await self.four.join_chat("goku_groupz")
+                await self.four.join_chat("channelz_k")
+                await self.four.join_chat("bklok")
 
             except:
                 pass
@@ -93,10 +93,10 @@ class Userbot(Client):
         if config.STRING2:
             await self.two.start()
             try:
-                await self.two.join_chat("TheShivanshu")
-                await self.two.join_chat("TheChampuClub")
-                await self.two.join_chat("akaChampu")
-                await self.two.join_chat("TheChampu")
+                await self.four.join_chat("LOVING_SOCIETY")
+                await self.four.join_chat("goku_groupz")
+                await self.four.join_chat("channelz_k")
+                await self.four.join_chat("bklok")
             except:
                 pass
             assistants.append(2)
@@ -117,10 +117,10 @@ class Userbot(Client):
         if config.STRING3:
             await self.three.start()
             try:
-                await self.three.join_chat("TheShivanshu")
-                await self.three.join_chat("TheChampuClub")
-                await self.three.join_chat("akaChampu")
-                await self.three.join_chat("TheChampu")
+                await self.four.join_chat("LOVING_SOCIETY")
+                await self.four.join_chat("goku_groupz")
+                await self.four.join_chat("channelz_k")
+                await self.four.join_chat("bklok")
             except:
                 pass
             assistants.append(3)
@@ -142,10 +142,10 @@ class Userbot(Client):
         if config.STRING4:
             await self.four.start()
             try:
-                await self.four.join_chat("TheShivanshu")
-                await self.four.join_chat("TheChampuClub")
-                await self.four.join_chat("akaChampu")
-                await self.four.join_chat("TheChampu")
+                await self.four.join_chat("LOVING_SOCIETY")
+                await self.four.join_chat("goku_groupz")
+                await self.four.join_chat("channelz_k")
+                await self.four.join_chat("bklok")
             except:
                 pass
             assistants.append(4)
