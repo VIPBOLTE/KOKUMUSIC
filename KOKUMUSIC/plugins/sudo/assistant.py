9a8fc33caf203fb8b6234c2877166a146825dd72
MR GOKU <154590485+VIPBOLTE@users.noreply.github.com>
2025-02-14 00:56:00 +0530

    Update assistant.py

--- KOKUMUSIC/plugins/sudo/assistant.py
+++ KOKUMUSIC/plugins/sudo/assistant.py
@@ -4,14 +4,14 @@ from inspect import getfullargspec
 from pyrogram import filters
 from pyrogram.types import Message
 
-from ChampuMusic import app
-from ChampuMusic.misc import SUDOERS
-from ChampuMusic.utils.database import get_client
+from KOKUMUSIC import app
+from KOKUMUSIC.misc import SUDOERS
+from KOKUMUSIC.utils.database import get_client
 
 
 @app.on_message(filters.command("setpfp", prefixes=".") & SUDOERS)
 async def set_pfp(client, message):
-    from ChampuMusic.core.userbot import assistants
+    from KOKUMUSIC.core.userbot import assistants
 
     if not message.reply_to_message or not message.reply_to_message.photo:
         return await eor(message, text="Reply to a photo")
@@ -29,7 +29,7 @@ async def set_pfp(client, message):
 
 @app.on_message(filters.command("setbio", prefixes=".") & SUDOERS)
 async def set_bio(client, message):
-    from ChampuMusic.core.userbot import assistants
+    from KOKUMUSIC.core.userbot import assistants
 
     if len(message.command) == 1:
         return await eor(message, text="Give some text to set as bio.")
@@ -48,7 +48,7 @@ async def set_bio(client, message):
 
 @app.on_message(filters.command("setname", prefixes=".") & SUDOERS)
 async def set_name(client, message):
-    from ChampuMusic.core.userbot import assistants
+    from KOKUMUSIC.core.userbot import assistants
 
     if len(message.command) == 1:
         return await eor(message, text="Give some text to set as name.")
@@ -67,7 +67,7 @@ async def set_name(client, message):
 
 @app.on_message(filters.command("delpfp", prefixes=".") & SUDOERS)
 async def del_pfp(client, message):
-    from ChampuMusic.core.userbot import assistants
+    from KOKUMUSIC.core.userbot import assistants
 
     for num in assistants:
         client = await get_client(num)
@@ -84,7 +84,7 @@ async def del_pfp(client, message):
 
 @app.on_message(filters.command("delallpfp", prefixes=".") & SUDOERS)
 async def delall_pfp(client, message):
-    from ChampuMusic.core.userbot import assistants
+    from KOKUMUSIC.core.userbot import assistants
 
     for num in assistants:
         client = await get_client(num)
