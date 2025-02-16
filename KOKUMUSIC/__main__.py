34389c3773237f9f3eac8ea121d1210a21a6250f
MR GOKU <154590485+VIPBOLTE@users.noreply.github.com>
2025-02-17 00:05:06 +0530

    Update __main__.py

--- KOKUMUSIC/__main__.py
+++ KOKUMUSIC/__main__.py
@@ -6,7 +6,7 @@ from pyrogram import idle
 import config
 from config import BANNED_USERS
 from KOKUMUSIC import HELPABLE, LOGGER, app, userbot
-from KOKUMUSIC.core.call import KOKU  # Ensure KOKU is initialized correctly
+from KOKUMUSIC.core.call import KOKU
 from KOKUMUSIC.plugins import ALL_MODULES
 from KOKUMUSIC.utils.database import get_banned_users, get_gbanned
 
@@ -20,14 +20,16 @@ async def init():
         and not config.STRING5
     ):
         LOGGER("KOKUMUSIC").error(
-            "No Assistant Clients Vars Defined!.. Exiting Process."
+            "ᴀssɪsᴛᴀɴᴛ ᴄʟɪᴇɴᴛ ᴠᴀʀɪᴀʙʟᴇs ɴᴏᴛ ᴅᴇғɪɴᴇᴅ, ᴇxɪᴛɪɴɢ..."
         )
         return
     if not config.SPOTIFY_CLIENT_ID and not config.SPOTIFY_CLIENT_SECRET:
         LOGGER("KOKUMUSIC").warning(
-            "No Spotify Vars defined. Your bot won't be able to play spotify queries."
+            "ɴᴏ sᴘᴏᴛɪғʏ ᴠᴀʀs ᴅᴇғɪɴᴇᴅ. ʏᴏᴜʀ ʙᴏᴛ ᴡᴏɴ'ᴛ ʙᴇ ᴀʙʟᴇ ᴛᴏ ᴘʟᴀʏ sᴘᴏᴛɪғʏ ǫᴜᴇʀɪᴇs..."
         )
 
+    await app.start()
+    await userbot.start()
     try:
         users = await get_gbanned()
         for user_id in users:
@@ -38,34 +40,20 @@ async def init():
     except Exception:
         pass
 
-    # Start Pyrogram client
-    await app.start()
-    LOGGER("KOKUMUSIC").info("Pyrogram client started!")
-
-    # Import all modules
     for all_module in ALL_MODULES:
         imported_module = importlib.import_module(all_module)
+
         if hasattr(imported_module, "__MODULE__") and imported_module.__MODULE__:
             if hasattr(imported_module, "__HELP__") and imported_module.__HELP__:
                 HELPABLE[imported_module.__MODULE__.lower()] = imported_module
-    LOGGER("KOKUMUSIC.plugins").info("Successfully Imported All Modules ")
+    LOGGER("KOKUMUSIC.plugins").info("sᴜᴄᴄᴇssғᴜʟʟʏ ɪᴍᴘᴏʀᴛᴇᴅ ᴍᴏᴅᴜʟᴇs...")
 
-    # Start userbot
-    await userbot.start()
-    LOGGER("KOKUMUSIC").info("Userbot started!")
-
-    # Start pytgcalls
     await KOKU.start()
-    LOGGER("KOKUMUSIC").info("PyTgCalls started!")
-
-    # Run decorators (if any)
     await KOKU.decorators()
-    LOGGER("KOKUMUSIC").info("KOKUMUSIC STARTED SUCCESSFULLY 🕊️")
-
-    # Idle
+    LOGGER("ChampuMusic").info("\x43\x68\x61\x6D\x70\x75\x20\x42\x6F\x74\x20\x68\x61\x73\x20\x62\x65\x65\x6E\x20\x73\x75\x63\x63\x65\x73\x73\x66\x75\x6C\x6C\x79\x20\x73\x74\x61\x72\x74\x65\x64\x2E\x0A\x0A\x40\x54\x68\x65\x43\x68\x61\x6D\x70\x75\x20")
     await idle()
 
 
 if __name__ == "__main__":
     asyncio.get_event_loop_policy().get_event_loop().run_until_complete(init())
-    LOGGER("KOKUMUSIC").info("Stopping KOKUMUSIC! GoodBye")
+    LOGGER("KOKUMUSIC").info("sᴛᴏᴘᴘɪɴɢ ᴄʜᴀᴍᴘᴜᴍᴜsɪᴄ! ɢᴏᴏᴅʙʏᴇ")
