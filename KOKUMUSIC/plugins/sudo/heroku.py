1eb7e2f39eaf0d7c2c0cd0753b3c64624f86272e
MR GOKU <154590485+VIPBOLTE@users.noreply.github.com>
2025-02-16 13:39:00 +0530

    Update heroku.py

--- KOKUMUSIC/plugins/sudo/heroku.py
+++ KOKUMUSIC/plugins/sudo/heroku.py
@@ -331,7 +331,7 @@ async def restart_(_, message):
     await response.edit_text(
         "» ʀᴇsᴛᴀʀᴛ ᴘʀᴏᴄᴇss sᴛᴀʀᴛᴇᴅ, ᴘʟᴇᴀsᴇ ᴡᴀɪᴛ ғᴏʀ ғᴇᴡ sᴇᴄᴏɴᴅs ᴜɴᴛɪʟ ᴛʜᴇ ʙᴏᴛ sᴛᴀʀᴛs..."
     )
-    os.system(f"kill -9 {os.getpid()} && python3 -m ChampuMusic")
+    os.system(f"kill -9 {os.getpid()} && python3 -m KOKUMUSIC")
 
 
 import requests
