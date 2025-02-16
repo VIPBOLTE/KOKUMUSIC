bf1063af5668130883f16d76ea026b23bc8f49c9
MR GOKU <154590485+VIPBOLTE@users.noreply.github.com>
2025-02-16 14:48:40 +0530

    Update oneword.py

--- KOKUMUSIC/plugins/raid/oneword.py
+++ KOKUMUSIC/plugins/raid/oneword.py
@@ -7,8 +7,8 @@ from pyrogram.types import Message
 from pyrogram import filters, Client
 
 # import 
-from BADMUSIC.misc import SUDOERS as SUDO_USER
-from BADMUSIC.cplugin.utils.data import RAID, PBIRAID, OneWord, HIRAID, PORM, EMOJI, GROUP, VERIFIED_USERS
+from KOKUMUSIC.misc import SUDOERS as SUDO_USER
+from KOKUMUSIC.cplugin.utils.data import RAID, PBIRAID, OneWord, HIRAID, PORM, EMOJI, GROUP, VERIFIED_USERS
 
 
 
