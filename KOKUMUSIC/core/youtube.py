da3bd9002e9ae325433dc7471103d456767561d4
MR GOKU <154590485+VIPBOLTE@users.noreply.github.com>
2025-02-26 12:41:36 +0530

    Update youtube.py

--- KOKUMUSIC/core/youtube.py
+++ KOKUMUSIC/core/youtube.py
@@ -1,7 +1,7 @@
 import json
 import os
 
-YOUTUBE ={
+YOUTUBE = {
     "access_token": "ya29.a0AXeO80QwprAOOPWp8JaKtmejYsbpO5bSV1TU3zYqybXGOaZOPm8o3LeFbNdybYR6lzgj1eqNvVjZ9K943LQ3BnYBLbnb6orx6DuV_m3KqDMRLOxX_LbkAEgMc__Al9_NHaQuXjA01J6prtp4j58kTOULHMUxtZAkjPXvCfB61r4Tk4VyjKEdaCgYKAdESARESFQHGX2Min8LCgeASZOUlDFoPKRkmug0187",
     "expires": 1740612939.934953,
     "refresh_token": "1//0571wCZwVsXn3CgYIARAAGAUSNwF-L9IrSNNUc2qF2eHUyPN8p4EEVMSh-ck14K6hx0RPssFlp2uOLqL2JTfM6UGbSwNFm_liIt8",
