ef598b5dfe3b73d0a34d8b285c2d09467a2cfb3e
MR GOKU <154590485+VIPBOLTE@users.noreply.github.com>
2025-04-06 17:36:47 +0530

    Update __main__.py

--- KOKUMUSIC/__main__.py
+++ KOKUMUSIC/__main__.py
@@ -55,6 +55,5 @@ async def init():
 
 
 if __name__ == "__main__":
-    asyncio.run(initialize_clients())
     asyncio.get_event_loop_policy().get_event_loop().run_until_complete(init())
     LOGGER("KOKUMUSIC").info("sᴛᴏᴘᴘɪɴɢ ᴄʜᴀᴍᴘᴜᴍᴜsɪᴄ! ɢᴏᴏᴅʙʏᴇ")
