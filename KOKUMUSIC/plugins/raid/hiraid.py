5930f4fb61b8a6ed82b40a483bca2152be3c1fe1
MR GOKU <154590485+VIPBOLTE@users.noreply.github.com>
2025-04-06 15:41:37 +0530

    Update hiraid.py

--- KOKUMUSIC/plugins/raid/hiraid.py
+++ KOKUMUSIC/plugins/raid/hiraid.py
@@ -5,7 +5,7 @@ from random import choice
 from pyrogram.errors import FloodWait
 from pyrogram.types import Message
 from pyrogram import filters, Client
-
+import asyncio
 # import 
 from KOKUMUSIC.misc import SUDOERS as SUDO_USER
 from KOKUMUSIC.cplugin.utils.data import RAID, PBIRAID, OneWord, HIRAID, PORM, EMOJI, GROUP, VERIFIED_USERS
