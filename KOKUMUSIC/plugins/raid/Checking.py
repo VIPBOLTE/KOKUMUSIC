785e7c4ce5df3a8c6d489973302a4f94308ef3cc
MR GOKU <154590485+VIPBOLTE@users.noreply.github.com>
2025-04-11 13:02:08 +0530

    Update Checking.py

--- KOKUMUSIC/plugins/raid/Checking.py
+++ KOKUMUSIC/plugins/raid/Checking.py
@@ -11,3 +11,31 @@ from pyrogram import filters, Client
 from KOKUMUSIC.misc import SUDOERS as SUDO_USER
 from KOKUMUSIC.cplugin.utils.data import RAID, PBIRAID, OneWord, HIRAID, PORM, EMOJI, GROUP, VERIFIED_USERS
 
+@Client.on_message(filters.command("emojii", prefixes=".") & SUDO_USER)
+async def emoji(x: Client, e: Message):
+      PBX = "".join(e.text.split(maxsplit=1)[1:]).split(" ", 2)
+
+      if len(PBX) == 2:
+          ok = await x.get_users(PBX[1])
+          counts = int(PBX[0])
+          for _ in range(counts):
+                reply = choice(EMOJI)
+                msg = f"[{ok.first_name}](tg://user?id={ok.id}) {reply}"
+                await x.send_message(e.chat.id, msg)
+                await asyncio.sleep(0.1)
+
+      elif e.reply_to_message:
+          user_id = e.reply_to_message.from_user.id
+          ok = await x.get_users(user_id)
+          counts = int(PBX[0])
+          for _ in range(counts):
+                reply = choice(EMOJI)
+                msg = f"[{ok.first_name}](tg://user?id={ok.id}) {reply}"
+                await x.send_message(e.chat.id, msg)
+                await asyncio.sleep(0.1)
+
+      else:
+            await e.reply_text(".emojii 10 <ʀᴇᴘʟʏ ᴛᴏ ᴜꜱᴇʀ ᴏʀ ᴜꜱᴇʀɴᴀᴍᴇ>")
+            
+
+              
